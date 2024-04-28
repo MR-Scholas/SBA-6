33b9a38b6425826ffce8005138812d1c4553cc87
MR-Scholas <160638944+MR-Scholas@users.noreply.github.com>
2024-04-28 14:19:26 -0400

    comments route finished

--- routes/comments.mjs
+++ routes/comments.mjs
@@ -34,6 +34,17 @@ router
         if (!result) res.send("Not found").status(404)
         else res.send(result).status(200)
     })
+    .patch(async (req,res)=>
+    {
+        let collection=await db.collection("comments")
+        let query={_id: new ObjectId(req.params.id)}
+        
+        let result=await collection.updateOne
+        (query, {$set:{content:String(req.body.content)}})
+
+        if (!result) res.send("Not found").status(404)
+        else res.send(result).status(200)
+    })
     .delete(async (req,res)=>
     {
         let collection=await db.collection("comments")
